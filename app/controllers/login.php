b6618670394d02ab977429698a506d4c1b787950
Mazinbajunaid <32253017+Mazinbajunaid@users.noreply.github.com>
2017-11-06 21:34:39 -0500

    added new coded

--- app/controllers/login.php
+++ app/controllers/login.php
@@ -1,7 +1,9 @@
 <?php
 
-class Login extends Controller {
-    public function index() {
+class Login extends Controller 
+{
+    public function index()
+	{
         $user = $this->model('User');
 
         if (in_array($_POST['UserName'], $LoginInfo))
@@ -39,7 +41,9 @@ class Login extends Controller {
 		{
         header('Location: /home');
     }
-	
+		}
+		}
+	}
 	public function register () {
 		$user = $this->model('User');
 		 
@@ -92,7 +96,7 @@ class Login extends Controller {
 				}		
 			}
 				
-			if (!($number && $lowercase && $upercase && $s ymbol))
+			if (!($number && $lowercase && $upercase && $symbol))
 			{
 				$_SESSION ['password_Okay'] = false;
 			}
@@ -107,16 +111,64 @@ class Login extends Controller {
 				
 				
 				
-				
-				
-	/*	
-		if(isset($_POST['save'])){
-   $name=$_POST['username'];
-   $email=$_POST['email'];
-   $pass1=$_POST['password'];
-   $hash=password_hash($pass1,PASSWORD_DEFAULT);
-		}
-		*/
+	
+    
+    include("connection.php");
+ 
+    if(isset($_POST['submit'])) {
+        $name = $_POST['username'];
+        $email = $_POST['email'];
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+ 
+        if($user == "" || $pass == "" || $name == "" || $email == "") {
+            echo "All fields should be filled. Either one or many fields are empty.";
+            echo "<br/>";
+            echo "<a href='register.php'>Go back</a>";
+        } else {
+            mysqli_query($mysqli, "INSERT INTO login(name, email, username, password) VALUES('$name', '$email', '$user', md5('$pass'))")
+            or die("Could not execute the insert query.");
+            
+            echo "Registration successfully";
+            echo "<br/>";
+            echo "<a href='login.php'>Login</a>";
+        }
+    } else {
+
+        <p><font size="+2">Register</font></p>
+        <form name="form1" method="post" action="">
+            <table width="75%" border="0">
+                <tr> 
+                    <td width="10%">Full Name</td>
+                    <td><input type="text" name="name"></td>
+                </tr>
+                <tr> 
+                    <td>Email</td>
+                    <td><input type="text" name="email"></td>
+                </tr>            
+                <tr> 
+                    <td>Username</td>
+                    <td><input type="text" name="username"></td>
+                </tr>
+                <tr> 
+                    <td>Password</td>
+                    <td><input type="password" name="password"></td>
+                </tr>
+                <tr> 
+                    <td>&nbsp;</td>
+                    <td><input type="submit" name="submit" value="Submit"></td>
+                </tr>
+            </table>
+        </form>
+    <?php
+    }
+    ?>
+
 		$this->view('home/register');
+	}
+}
+			}
+			
+		}
 	}
 }
